653ebd590870122963c8c435aa4d551fdc5b62a6
Karthikp <karthikkannanp2625@gmail.com>
2025-05-09 23:36:14 +0530

    Added custom code to create a superuser

--- office/office/settings.py
+++ office/office/settings.py
@@ -133,3 +133,21 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+# --- Temporary superuser creation for Render.com free tier ---
+from django.contrib.auth import get_user_model
+
+if os.environ.get("CREATE_SUPERUSER", "False") == "True":
+    def create_superuser():
+        User = get_user_model()
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+    import django
+    django.setup()
+    create_superuser()
+# --- End of temporary code ---
